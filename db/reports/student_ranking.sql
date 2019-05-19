f4738dd257e2295c1f03881d3fbdf2050a050d0b
Georgi D. Sotirov <gdsotirov@dir.bg>
2019-05-19 20:16:43 +0300

    Add report for raking student based on last year minimal rankings

---
+++ db/reports/student_ranking.sql
@@ -0,0 +1,38 @@
+SELECT ST.`name`, SC.short_name, CL.`name`, DS.`name`,
+       CL.coef_bel * ST.score_nea_bel +
+       CL.coef_mat * ST.score_nea_mat +
+       mark_to_score(SUBJ1.mark) +
+       mark_to_score(SUBJ2.mark) total_score,
+       CR.min_rank_I,
+       CR.min_rank_II
+  FROM classes        CL,
+       class_rankings CR,
+       schools        SC,
+       cities         CT,
+       districts      DS,
+       students       ST,
+       LATERAL
+       (SELECT subject_id, mark
+          FROM subj_marks
+		 WHERE student_id = ST.id
+           AND subject_id = CL.subj1_id
+       ) SUBJ1,
+       LATERAL
+       (SELECT subject_id, mark
+          FROM subj_marks
+		 WHERE student_id = ST.id
+           AND subject_id = CL.subj2_id
+       ) SUBJ2
+ WHERE CL.school_id = SC.id
+   AND CR.class_id = CL.id
+   AND SC.city_id = CT.id
+   AND SC.district_id = DS.id
+   AND CL.yr = YEAR(CURDATE())
+   AND CR.yr = YEAR(CURDATE()) - 1
+   AND CT.id = 1000 /* Sofia */
+   AND ST.id = 1
+ /*AND CL.coef_bel * ST.score_nea_bel +
+       CL.coef_mat * ST.score_nea_mat +
+       mark_to_score(SUBJ1.mark) +
+       mark_to_score(SUBJ2.mark) >= CR.min_rank_II */
+ ORDER BY CR.min_rank_II DESC;
