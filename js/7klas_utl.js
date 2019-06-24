078bb679552a41ff8779ac4cab28c5cad1702d2b
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-06-24 20:24:35 +0300

    Add functions round and score_to_mark

--- js/7klas_utl.js
+++ js/7klas_utl.js
@@ -3,6 +3,16 @@
 var $7klas = {}
 
 /**
+ * Round to given number of places
+ * @param number Number to round
+ * @param places Places to round to
+ * @returns Number rounded
+ */
+$7klas.round = function (number, places) {
+  return Math.round(number * Math.pow(10, places)) / Math.pow(10, places);
+}
+
+/**
  * Convert mark to scores (see ???)
  * @param mark Mark in six-point marking system
  * @returns Scores
@@ -23,4 +33,30 @@ $7klas.mark_to_score = function (mark) {
   }
 
   return score;
+}
+
+/**
+ * Convert scores to mark (see ???)
+ * @param score Scores from exam
+ * @returns Mark in six-point marking system
+ */
+$7klas.score_to_mark = function (score) {
+  var mark = 0.0;
+
+  if ( isNaN(score) ) {
+    throw "Score is NOT a number!";
+  }
+
+  if ( score < 0 || score > 100 ) {
+    throw "Score should be betweeen 0 and 100!";
+  }
+
+  if (score <= 15) {
+    mark = 2.0;
+  }
+  else {
+    mark = $7klas.round((score + 68)/28, 3);
+  }
+
+  return mark;
 }
