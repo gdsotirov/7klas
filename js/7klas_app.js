38ca0e5d3dd56cb9d6a9ca39623a674f42b8d7fa
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-02-14 21:56:26 +0200

    Transform to Vue application

--- js/7klas_app.js
+++ js/7klas_app.js
@@ -1,3 +1,23 @@
 /* 7klas Application */
 
-var app = angular.module('7klas_app', []);
+const app = Vue.createApp({
+    setup() {
+        var class_ranks_yrs = Vue.ref(["2019"])
+        var edit = true
+        var show_form = false
+
+        function addStudent() {
+            edit = true
+            show_form = true
+        }
+
+        return {
+            addStudent,
+            class_ranks_yrs,
+            edit,
+            show_form
+        }
+    }
+})
+
+app.mount('#app_7klas')
