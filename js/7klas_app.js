1a74775b535565834a5b73a8308a1b7294ee549c
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-02-15 08:59:13 +0200

    Add and bind form fields

--- js/7klas_app.js
+++ js/7klas_app.js
@@ -4,7 +4,17 @@ const app = Vue.createApp({
     setup() {
         var class_ranks_yrs = Vue.ref(["2019"])
         var edit = Vue.ref(true)
+        var rnkYear = Vue.ref(new Date().getFullYear().toString())
         var show_form = Vue.ref(false)
+        var stNEABEL =  Vue.ref('')
+        var stNEABEL_mul =  Vue.ref(1)
+        var stNEAMAT =  Vue.ref('')
+        var stNEAMAT_mul =  Vue.ref(3)
+        var stName = Vue.ref('')
+        var stRank =  Vue.ref('')
+        var stRankBy =  Vue.ref('both')
+        var stSubj1 =  Vue.ref('')
+        var stSubj2 =  Vue.ref('')
 
         function addStudent() {
             edit.value = true
@@ -20,7 +30,17 @@ const app = Vue.createApp({
             cancel,
             class_ranks_yrs,
             edit,
-            show_form
+            rnkYear,
+            show_form,
+            stNEABEL,
+            stNEABEL_mul,
+            stNEAMAT,
+            stNEAMAT_mul,
+            stName,
+            stRank,
+            stRankBy,
+            stSubj1,
+            stSubj2
         }
     }
 })
