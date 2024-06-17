5a443976d4ad190d85646ecb5c1cb96c4df13085
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-06-17 19:53:23 +0300

    Fix spelling error in comment

--- db/tables/students.sql
+++ db/tables/students.sql
@@ -1,8 +1,8 @@
 CREATE TABLE students (
   id            INT         NOT NULL AUTO_INCREMENT,
   name          VARCHAR(64) NOT NULL COMMENT 'Full name',
-  score_nea_bel INT         NOT NULL COMMENT 'Score from National External Assesment BEL',
-  score_nea_mat INT         NOT NULL COMMENT 'Score from National External Assesment MAT',
+  score_nea_bel INT         NOT NULL COMMENT 'Score from National External Assessment BEL',
+  score_nea_mat INT         NOT NULL COMMENT 'Score from National External Assessment MAT',
 
   PRIMARY KEY (id)
 )
