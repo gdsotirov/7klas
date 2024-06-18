49b6fd4c78529415d0ab5de51238310f5794a0fb
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-06-18 19:19:41 +0300

    Handle inconsistent classes data
    Handle inconsistent classes data between 1-st and 2-nd rankings, i.e.
    use outer join between the rankings tables.

--- db/data/load_data.sql
+++ db/data/load_data.sql
@@ -23,9 +23,10 @@ LOAD DATA INFILE '/var/mysql/files/sofia-20230719-minmax.csv'
 INSERT INTO classes
    (yr, school_id, id, `name`)
 SELECT r1.yr, r1.school_id, r1.class_id, r1.class_name
-  FROM min_max_1_in r1,
+  FROM min_max_1_in r1
+       LEFT OUTER JOIN
        min_max_2_in r2
- WHERE r1.yr        = r2.yr
+    ON r1.yr        = r2.yr
    AND r1.school_id = r2.school_id
    AND r1.class_id  = r2.class_id
 ON DUPLICATE KEY
@@ -35,9 +36,15 @@ INSERT INTO class_rankings
   (yr, class_id, min_rank_I, min_rank_II, max_rank_I, max_rank_II)
 SELECT r1.yr, r1.class_id,
        r1.min_rank AS `min_rank_I`,
-       r2.min_rank AS `min_rank_II`,
+       CASE
+         WHEN r2.min_rank IS NULL THEN 0.0
+         ELSE r2.min_rank
+       END         AS `min_rank_II`,
        r1.max_rank AS `max_rank_I`,
-       r2.max_rank AS `max_rank_II`
+       CASE
+         WHEN r2.max_rank IS NULL THEN 0.0
+         ELSE r2.min_rank
+       END AS `max_rank_II`
   FROM min_max_1_in r1
        LEFT OUTER JOIN
        min_max_2_in r2
