8dae92bd8a30184afaa6f783fab5f3a44cdb1f4c
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-06-18 18:56:47 +0300

    Primary key on class rankings should be on year and class
    Primary key on class rankings should be on year and class, so remove
    fictive auto increment identifier.

--- db/tables/class_rankings.sql
+++ db/tables/class_rankings.sql
@@ -1,5 +1,4 @@
 CREATE TABLE class_rankings (
-  id          INT           NOT NULL AUTO_INCREMENT,
   yr          YEAR          NOT NULL,
   class_id    INT           NOT NULL,
   min_rank_I  DECIMAL(5,2)  NOT NULL COMMENT 'Minimal rank from Ist ranking',
@@ -7,9 +6,7 @@ CREATE TABLE class_rankings (
   max_rank_I  DECIMAL(5,2)  NOT NULL COMMENT 'Maximal rank from Ist ranking',
   max_rank_II VARCHAR(45)   NOT NULL COMMENT 'Maximal rank from IInd ranking',
 
-  PRIMARY KEY (id),
-
-  INDEX fk_cr_class_idx (yr ASC, class_id ASC),
+  PRIMARY KEY (yr, class_id),
 
   CONSTRAINT fk_cr_class
     FOREIGN KEY (yr , class_id)
