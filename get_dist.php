fb5a7f5a62702a50f1e3568fbc743549b6f452b5
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-07-02 21:00:26 +0300

    Add script for getting list of school districts

---
+++ get_dist.php
@@ -0,0 +1,24 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$conn = new mysqli("localhost", "7klas_app", "7klas.APP", "7klas");
+
+$sql_result = $conn->query("SELECT * FROM SchoolDistrictsJSON");
+
+$json = "";
+while($rs = $sql_result->fetch_array(MYSQLI_ASSOC)) {
+  if ( $json != "" )
+  {
+    $json .= ",";
+  }
+  $json .= $rs["json_obj"];
+}
+
+/* Make array */
+$json = "[".$json."]";
+
+$conn->close();
+
+echo $json;
+?>
