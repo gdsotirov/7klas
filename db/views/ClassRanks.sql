f0d75f12f062c4bdff364a37df48d821eee5ff47
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-07-02 19:30:03 +0300

    Select also school district

--- db/views/ClassRanks.sql
+++ db/views/ClassRanks.sql
@@ -3,15 +3,18 @@ SELECT ROW_NUMBER() OVER(
          PARTITION BY CR.yr
          ORDER BY CR.yr, CR.min_rank_II DESC)       AS rnkNum,
        CAST(CR.yr AS CHAR(4))                       AS clsYear,
+       CONCAT(D.id, ' ', D.`name`)                  AS schlDist,
        CONCAT(SC.id, ' ', SC.short_name)            AS schlName,
        CONCAT(LPAD(CL.id, 4, '0'), ' ', CL.`name`)  AS clsName,
        CR.min_rank_I                                AS min_rank_I,
        CR.min_rank_II                               AS min_rank_II
   FROM classes        CL,
        class_rankings CR,
+       districts      D,
        schools        SC
- WHERE CR.class_id  = CL.id
-   AND CR.yr        = CL.yr
-   AND CL.school_id = SC.id
+ WHERE CR.class_id    = CL.id
+   AND CR.yr          = CL.yr
+   AND SC.district_id = D.id
+   AND CL.school_id   = SC.id
    AND ( CR.min_rank_I > 0 OR CR.min_rank_II > 0 )
  ORDER BY CR.yr, CR.min_rank_II DESC;
--- db/views/ClassRanksJSON.sql
+++ db/views/ClassRanksJSON.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE VIEW ClassRanksJSON AS
 SELECT JSON_OBJECT("rnkNum"     , rnkNum,
                    "clsYear"    , clsYear,
+                   "schlDist"   , schlDist,
                    "schlName"   , schlName,
                    "clsName"    , clsName,
                    "min_rank_I" , min_rank_I,
