f227c9378b1862ee28c894f25f5fed17806e0b3b
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-07-04 19:31:25 +0300

    Order districts by name

--- db/views/SchoolDistrictsJSON.sql
+++ db/views/SchoolDistrictsJSON.sql
@@ -2,4 +2,5 @@ CREATE OR REPLACE VIEW SchoolDistrictsJSON AS
 SELECT JSON_OBJECT("distId"     , id,
                    "distName"   , `name`
                   ) json_obj
-  FROM districts;
+  FROM districts
+ ORDER BY `name`;
